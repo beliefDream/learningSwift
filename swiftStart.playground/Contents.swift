29d96b68654cd2ebaca34f7b1e24822afd59b404
huangJichao <jichao.huang@qingfengapp.com.cn>
2016-06-02 09:23:10 +0800

    学习闭包，递归

--- swiftStart.playground/Contents.swift
+++ swiftStart.playground/Contents.swift
@@ -176,3 +176,143 @@ swapTwoNumber(&number1, num2: &number2)
 let swapFunc: (inout  Int, inout Int) -> Void = swapTwoNumber
 swapFunc(&number1, &number2)
 
+
+
+
+
+///  7闭包
+
+// 7.1 sort
+let name7s = ["b", "a", "w", "q"]
+func backwards(s1:String, s2: String) -> Bool {
+    return s1 < s2
+}
+
+// 7.1.1 sort中 调用系统的方法  <#T##isOrderedBefore: (String, String) -> Bool##(String, String) -> Bool#>
+name7s.sort { (s11, s22) -> Bool in
+    return s11 > s22
+}
+
+name7s.sort { (s1: String, s2: String) -> Bool in
+    return s1 > s2;
+}
+
+// 7.1.2 sort 中调用  自定义的函数
+print( name7s.sort(backwards) )
+
+// 7.1.3 根据上下文推断类型
+var reversed = name7s.sort( {s1, s2 in return s1 > s2 } )
+
+// 7.1.4 单表达式闭包隐式 返回
+reversed = name7s.sort( {s1, s2 in s1 > s2 } )
+
+//// 7.1.5 参数名称缩写
+reversed = name7s.sort( {$0 > $1 })
+print(reversed)
+
+//  7.1.6 运算符函数
+reversed = name7s.sort( < )
+
+// 7.1.7  值捕获
+func makeIncrementor(forIncrement amount: Int) -> () ->Int {
+    var runningTotal = 0
+    func incrementor() ->Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementor
+}
+
+let incrementByTen = makeIncrementor(forIncrement: 10)
+print(incrementByTen)
+incrementByTen()
+let incrementBySeven = makeIncrementor(forIncrement: 7)
+incrementBySeven()
+
+incrementByTen()
+
+
+// 7.1.8 闭包是引用类型
+
+
+
+
+
+// 8 枚举
+// 8.1  枚举的写法1
+enum CompassPoint {
+    case North
+    case South
+    case West
+    case East
+}
+//  写法2
+enum CompassPoint2 {
+    case North, South, West, East
+}
+
+// 8.2 定义 枚举型变量
+var direction = CompassPoint.East
+
+direction = .North
+direction = .South
+switch  direction {
+case .North :
+    print("north")
+case .South :
+    print("south")
+case .West :
+    print("west")
+case .East :
+    print("east")
+}
+
+// 8.3   关联值  (较 常用的 枚举写法稍复杂些)
+enum Barcode {
+    case UPCA(Int, Int, Int, Int)
+    case QRCode(String)
+}
+
+var productBarcode = Barcode.UPCA(8, 111, 222, 3)
+productBarcode = .QRCode("ABCD")
+
+switch productBarcode {
+case .UPCA(let a, let b, let c, let d):
+    print("\(a), \(b), \(c), \(d)")
+case .QRCode(let code):
+    print(code)
+}
+
+switch productBarcode {
+case let .UPCA(a, b, c, d):
+    print(a + b + c + d)
+case let .QRCode(code):
+    print(code)
+}
+
+
+//8.4 原始值
+enum ASCIICharacter: Character {
+    case Tab = "\t"
+    case LineFeed = "\n"
+    case Return = "\r"
+}
+
+// 8.4.1 原始值 的隐式赋值
+enum Planet: Int {
+    case Mercury = 1, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+}
+enum ComPassPoint: String {
+    case North, South, East, West
+}
+let earthsOrder = Planet.Earth.rawValue
+print(earthsOrder)
+
+let sunsetDirection = ComPassPoint.West.rawValue
+print(sunsetDirection)
+
+// 8.4.2 使用原始值 初始化 枚举实例
+let possiblePlanet = Planet(rawValue :7)
+print(possiblePlanet)
+
+// 8.5.3 枚举递归 
